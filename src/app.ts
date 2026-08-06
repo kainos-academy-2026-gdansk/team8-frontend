ab99f9bba77ec99ea36e77be183fda8ea563a8df
rafaltomaszczyk <rafal.tomaszczyk01@gmail.com>
2026-08-06 13:42:42 +0200

    US038 - Linting check (#2)
    * US038 - Linting check
    
    * added missing _req in app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ nunjucks.configure('src/views', {
     noCache: true,
 });
 
-app.get('/', function(req, res) {
+app.get('/', (_req, res) => {
     res.render('pages/index.njk', {message: 'Hello world!'});
 });
 
