a9b82e341b71cc9839e2031f569621a0ca905879
oskar-skomra <oskar.skomra@kainos.com>
2026-08-18 13:55:11 +0200

    change npm run to ci in e2e workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "npm run dev",
+		command: process.env.CI ? "npm run start" : "npm run dev",
 		url: baseURL,
 		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000,
