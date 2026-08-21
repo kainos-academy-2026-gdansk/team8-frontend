ba17b8e74154906753b0f281d33f5d5209f8874c
MaciejPilarski-kainos <maciej.pilarski@kainos.com>
2026-08-21 15:52:42 +0200

    EDITED: WCAG and Accessibility improvement (#25)
    * EDITED: WCAG and Accessibility improvement
    
    * EDITED: Fixed contrast errors
    
    * Tests/pagination e2e ghcr (#24)
    
    * Added pagination tests
    
    * added pagination mocks
    
    * Added pagination e2e tests and CI nows starts team8-backend pacage form GHCR
    
    * changed e2e.yml to remove master
    
    * Fixed EOF
    
    * Changed token for GHCR
    
    * Added jwt_secret to e2e.yml
    
    * Added trailing newline
    
    * FIXED: CI error fix - test
    
    ---------
    
    Co-authored-by: Szymon Sztreja <91730827+szymonsztreja@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,6 @@
 import path from "node:path";
 import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
-import { defineBddConfig } from "playwright-bdd";
 
 dotenv.config({ path: path.resolve(__dirname, ".env"), quiet: true });
 
