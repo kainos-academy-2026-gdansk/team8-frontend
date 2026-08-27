8fd797531464dcabcff98efa9154910384cdaf3c
oskar-skomra <oskar.skomra@kainos.com>
2026-08-27 09:34:47 +0200

    changed continer arch to linux

--- my_infrastructure/outputs.tf
+++ my_infrastructure/outputs.tf
@@ -17,3 +17,33 @@ output "storage_account_name" {
   description = "Name of the created storage account"
   value       = azurerm_storage_account.this.name
 }
+
+output "database_server_fqdn" {
+  description = "Fully qualified domain name of the PostgreSQL server"
+  value       = azurerm_postgresql_flexible_server.this.fqdn
+}
+
+output "database_name" {
+  description = "Name of the application database"
+  value       = azurerm_postgresql_flexible_server_database.this.name
+}
+
+output "container_apps_identity_id" {
+  description = "Resource ID of the managed identity used by Container Apps"
+  value       = azurerm_user_assigned_identity.container_apps.id
+}
+
+output "container_apps_identity_principal_id" {
+  description = "Principal ID of the managed identity used by Container Apps"
+  value       = azurerm_user_assigned_identity.container_apps.principal_id
+}
+
+output "frontend_container_app_url" {
+  description = "Public URL of the frontend Container App"
+  value       = "https://${azurerm_container_app.frontend.ingress[0].fqdn}"
+}
+
+output "backend_container_app_fqdn" {
+  description = "Internal FQDN of the backend Container App"
+  value       = azurerm_container_app.backend.ingress[0].fqdn
+}
--- my_infrastructure/variables.tf
+++ my_infrastructure/variables.tf
@@ -38,6 +38,56 @@ variable "storage_account_name" {
   }
 }
 
+variable "database_name" {
+  description = "Name of the application database"
+  type        = string
+  default     = "team8_backend"
+}
+
+variable "database_admin_username" {
+  description = "Administrator username for PostgreSQL"
+  type        = string
+  default     = "team8admin"
+}
+
+variable "database_admin_password" {
+  description = "Administrator password for PostgreSQL; provide through TF_VAR_database_admin_password"
+  type        = string
+  sensitive   = true
+}
+
+variable "container_registry_id" {
+  description = "Resource ID of the existing Azure Container Registry"
+  type        = string
+  default     = null
+  nullable    = true
+}
+
+variable "container_registry_login_server" {
+  description = "Login server of the existing Azure Container Registry"
+  type        = string
+  default     = null
+  nullable    = true
+}
+
+variable "frontend_image" {
+  description = "Frontend image repository and tag in ACR"
+  type        = string
+  default     = "team8-frontend:latest"
+}
+
+variable "backend_image" {
+  description = "Backend image repository and tag in ACR"
+  type        = string
+  default     = "team8-backend:latest"
+}
+
+variable "feature_flag_enabled" {
+  description = "Feature flag passed to the frontend Container App"
+  type        = bool
+  default     = false
+}
+
 variable "tags" {
   description = "Common tags applied to all resources"
   type        = map(string)
